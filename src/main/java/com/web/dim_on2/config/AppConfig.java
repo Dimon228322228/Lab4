4bddae65f716ae24de0ea08f5745729cc0029bbb
Dimon322228322 <lazdimasik@gmail.com>
2023-01-29 16:42:52 +0300

    Some fix

---
+++ src/main/java/com/web/dim_on2/config/AppConfig.java
@@ -0,0 +1,18 @@
+package com.web.dim_on2.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.method.HandlerTypePredicate;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+@PropertySource("classpath:/application.properties")
+@PropertySource("classpath:/db.properties")
+public class AppConfig implements WebMvcConfigurer {
+}
